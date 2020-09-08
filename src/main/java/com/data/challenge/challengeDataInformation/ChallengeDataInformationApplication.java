386ab7d46a62077f690a9da1402b1283fdbbcdfc
JulioMoliveira <julio.juliooliveira@gmail.com>
2020-09-08 18:51:18 -0300

    work structure

---
+++ src/main/java/com/data/challenge/challengeDataInformation/ChallengeDataInformationApplication.java
@@ -0,0 +1,13 @@
+package com.data.challenge.challengeDataInformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeDataInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeDataInformationApplication.class, args);
+	}
+
+}
